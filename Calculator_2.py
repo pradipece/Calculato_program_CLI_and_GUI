f47d60c297fb22921cf0ac0baeda1e61f4507891
pradipece <pradipnathece@gmail.com>
2024-11-08 21:25:30 +0600

    version 1.0.0

---
+++ Calculator_2.py
@@ -0,0 +1,50 @@
+# Advance level Calculator Program
+print("Welcome to calculator program")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+
+# Function for the option
+def addition(num1,num2):
+    add = num1+num2
+    return add
+
+def subtraction(num1,num2):
+    sub = num1-num2
+    return sub
+
+def multiplication(num1,num2):
+    mul = num1*num2
+    return mul
+
+def division(num1,num2):
+    div = num1-num2
+    return div
+
+#use while loop
+while True:
+    option = input("Select an option for calculation option(+ , - , * , / ):  ")
+    match option:
+        case "+":
+            num1 = int(input("Enter the 1st number: "))
+            num2 = int(input("Enter the 2nd number: "))  
+            print("Addition is: ", addition(num1,num2)) 
+        
+        case "-":
+            num1 = int(input("Enter the 1st number: "))
+            num2 = int(input("Enter the 2nd number: "))  
+            print("Subtraction is: ", subtraction(num1,num2)) 
+        
+        case "*":
+            num1 = int(input("Enter the 1st number: "))
+            num2 = int(input("Enter the 2nd number: "))  
+            print("Multiplication is: ", multiplication(num1,num2)) 
+        
+        case "/":
+            num1 = int(input("Enter the 1st number: "))
+            num2 = int(input("Enter the 2nd number: "))  
+            print("Addition is: ", division(num1,num2)) 
+        case 5:
+            break
+           
